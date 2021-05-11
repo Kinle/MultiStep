668964e95c54e79305a8c1b7250cc8ccf7116229
Kinle <kinle.greshka.work@gmail.com>
2021-05-11 16:39:46 +0530

    [Kinle] Removed unnecessary dependencies

---
+++ dist/MultiStep.d.ts
@@ -0,0 +1,29 @@
+import { Step } from './Step';
+import '../scss/MultiStep';
+import '../scss/StepContent';
+import '../scss/Actions';
+declare type MultiStepOptions = {
+    onComplete?: () => void;
+    onNext?: (step: Step) => void;
+    onPrev?: (step: Step) => void;
+    nextLabel: string;
+    prevLabel: string;
+    completeLabel: string;
+};
+export interface Containers {
+    mainContainer: HTMLElement;
+    progressContainer: HTMLElement;
+    stepContainer: HTMLElement;
+    actionContainer: HTMLElement;
+}
+interface IMultiStep {
+    updateOptions(newOptions: MultiStepOptions): void;
+    addStep(): Step;
+    getStep(step: number): Step | undefined;
+    reset(): void;
+}
+declare const MultiStep: {
+    (target: HTMLElement, options?: MultiStepOptions | undefined): IMultiStep;
+    get(element: HTMLElement): IMultiStep | undefined;
+};
+export default MultiStep;
